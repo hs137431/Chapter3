dbfcc5f9c4efe5676bafd8ebc872a3b834a3a205
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-09-11 09:48:32 -0500

    First tutorials

---
+++ src/tutorial/First.java
@@ -0,0 +1,12 @@
+package tutorial;
+
+public class First {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("What is your address?");
+		Second.displayAddress();
+	
+	}
+}
---
+++ src/tutorial/Second.java
@@ -0,0 +1,19 @@
+package tutorial;
+
+public class Second {
+
+	
+		// TODO Auto-generated method stub
+		public static void displayAddress()
+		{
+		System.out.println("XYZ Company");
+		System.out.println("8900 U.S. Hwy 15");
+		System.out.println("Crystal Lake, IL 60014");
+		System.out.println("First Java Application");
+		}
+		
+		
+	}
+	
+
+
