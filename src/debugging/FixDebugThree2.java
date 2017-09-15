e81d65de4270f39b9d8fe0d89a7eae3c2c39b3b8
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-09-15 08:37:17 -0500

    Debuggings Completed

---
+++ src/debugging/FixDebugThree2.java
@@ -0,0 +1,22 @@
+package debugging;
+// This application displays some math facts
+public class FixDebugThree2
+{
+   public static void main(String args[])
+   {
+      double a = 2, b = 5, c = 10;
+      add(a, b);
+      add(b, c);
+      subtract(c, a);            
+   }
+   public static void add(double a, double  b)
+   {
+      System.out.println("The sum of " + a +
+         " and " + b + " is " + (a + b));
+   }
+   public static void subtract(double a, double b)
+   {
+      System.out.println("The difference between " +
+        a + " and " + b + " is " +  (a - b));
+   }
+}
